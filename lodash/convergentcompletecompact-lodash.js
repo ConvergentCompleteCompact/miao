0c86ac1e4cfdfcca423ecc03d257e63514889d03
ConvergentCompleteCompact <58259783+ConvergentCompleteCompact@users.noreply.github.com>
2020-05-10 08:53:38 +0800

    Update convergentcompletecompact-lodash.js

--- lodash/convergentcompletecompact-lodash.js
+++ lodash/convergentcompletecompact-lodash.js
@@ -1,1 +1,16 @@
-
+var ConvergentCompleteCompact = {
+  isNull: function(val) {
+    if (val === null) {
+      return true
+    } else {
+      return false
+    }
+  },
+  isNaN: function(val) {
+    is (val !== val) {
+      return true
+    } else {
+      return false 
+    }
+  },
+}
